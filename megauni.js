457b7242a7551b554aa57136961f637168139b78
da99 <i-hate-spam-1234567@mailinator.com>
2015-07-26 22:26:33 -0500

    Refactoring

--- megauni.js
+++ megauni.js
@@ -8,44 +8,74 @@ var MegaUni = {
 
 $(function () {
 
-  var show_if = function (raw) {
-    var node = $(raw);
-    var val  = node.attr('show_if');
-    node.removeAttr('show_if');
-    node.hide();
-
-    var id = Applet.id(node);
-    MegaUni.run(function (data) {
-      var ans = Applet.is_true(data, val);
-      if (ans === undefined)
-        return;
-
-      if ( ans )
-        $('#' + id).show();
-      else
-        $('#' + id).hide();
-    });
-  }; // === show_if
-
   var process_node = MegaUni.run_node = function (script) {
 
-    _.each($(script).find('*[show_if]'), show_if);
+    _.each(MegaUni.stack, function (o) {
+      if (o.name !== 'attr')
+        return;
+      _.each($(script).find('*[' + o.attr + ']'), o.func);
+    });
+
 
     ($(script).contents()).insertBefore($(script));
   }; // === process_node
 
   MegaUni.run = function (func) {
-    // === Compile all scripts
-    //     before proceding.
+
+    // === Compile all scripts:
     Applet.run(process_node);
 
-    if (_.isFunction(func))
-      MegaUni.stack.push(func);
-    else if (_.isPlainObject(func)) {
+    if (_.isPlainObject(func))
       _.each(MegaUni.stack, function (f) {
-        f(func);
+        if (!f.name)
+          f(func);
       });
-    }
+
+    return MegaUni;
+
   }; // === func
 
+  var insert = function (pos, name, func) {
+    if (_.isFunction(name))
+      MegaUni.stack[pos](name);
+    else {
+      MegaUni.stack[pos]({name: 'attr', attr: name, func: func});
+    }
+
+    return MegaUni;
+  };
+
+   MegaUni.unshift = function (name, func) {
+    return insert('unshift', name, func);
+  };
+
+  var push = MegaUni.push = function (name, func) {
+    return insert('push', name, func);
+  };
+
+  push(
+    'show_if',
+    function (raw) {
+      var node = $(raw);
+      var val  = node.attr('show_if');
+      node.removeAttr('show_if');
+      node.hide();
+
+      var id = Applet.id(node);
+      MegaUni.push(function (data) {
+        var ans = Applet.is_true(data, val);
+        if (ans === undefined)
+          return;
+
+        if ( ans )
+          $('#' + id).show();
+        else
+          $('#' + id).hide();
+      });
+    } // === func
+  ); // === show_if
+
 }); // === scope
+
+
+
