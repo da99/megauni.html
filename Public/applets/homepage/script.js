3c1d60589b0d36bafd872b62bd67aa5fe1a89fdc
da99 <i-hate-spam-1234567@mailinator.com>
2015-08-24 10:59:15 -0500

    Dev. checkpoint.

--- Public/applets/homepage/script.js
+++ Public/applets/homepage/script.js
@@ -6,9 +6,31 @@ var APP;
 
 $(function () {
 
+  var form_success = function (o) {
+    if (o.name !== 'ajax response')
+      return;
+
+    if (o.response.error)
+      return;
+
+    var form = $('#' + o.request.form_id);
+    form.trigger('reset');
+
+    switch (o.request.form_id) {
+
+      case 'create_account':
+        form.find('div.success_msg').text('Account created. Please wait as page reloads...');
+        form.find('div.buttons').hide();
+        window.location.reload(true);
+        break;
+
+    } // === switch o.request.form_id
+  }; // === func
+
   APP = new Applet(
     _.values(Applet.funcs),
-    MegaUni.funcs.ui_ajax
+    MegaUni.funcs.ui_ajax,
+    form_success
   );
 
   APP.run(
--- Public/scripts/megauni.js
+++ Public/scripts/megauni.js
@@ -5,20 +5,19 @@ var MegaUni = {funcs: {}};
 
 $(function () { // === START SCOPE ======
 
-  // === CONSTRUCTOR ====
   MegaUni.funcs.ui_ajax = function (o) {
 
     // === Clear errors:
     if (o.name === 'dom') {
       $('input[type="text"], textarea').on('keypress', function () {
         $(this).closest('div.field').removeClass('field_invalid');
-        $(this).closest('div.form').find('div.error_msg').remove();
+        $(this).closest('div.form').find('div.error_msg, div.success_msg').remove();
       });
 
       $('form button.submit').on('click', function () {
         var form = $(this).closest('form');
         form.find('div.field_invalid').removeClass('field_invalid');
-        form.find('div.error_msg').remove();
+        form.find('div.error_msg, div.success_msg').remove();
       });
     }
 
@@ -28,25 +27,47 @@ $(function () { // === START SCOPE ======
       $('#' + o.form_id + ' div.buttons').after('<div class="loading_msg">Processing...</div>');
     }
 
-    // === Display errors:
     if (o.name === 'ajax response' && o.request.form_id) {
-      var div_form = $('#' + o.request.form_id).closest('div.form');
-      div_form.removeClass('loading');
-      $('#' + o.request.form_id + ' div.loading_msg').remove();
 
-      if (o.response.error && o.response.error.fields) {
-        _.each(o.response.error.fields, function (msg, key) {
+      var div_form = $('#' + o.request.form_id).closest('div.form');
+
+      div_form
+      .removeClass('loading')
+      .find('div.loading_msg')
+      .remove();
+
+      // === Display errors:
+      if (o.response.error) {
+
+        _.each(o.response.error.fields || [], function (msg, key) {
           var field = div_form.find('div.field.' + key + ':first');
           field.addClass('field_invalid');
           var err = $('<div class="error_msg error_' + key + '"></div>');
           err.text(msg);
           field.after(err);
         });
-      } // === if error fields
-    }
+
+      } // === end: display errors
+
+      // === Display success msg:
+      if (o.response.success) {
+        // === reset msg:
+        div_form
+        .addClass('form_success')
+        .find('div.success_msg')
+        .remove();
+
+        // === add msg:
+        div_form
+        .find('div.buttons')
+        .after(
+          $('<div class="success_msg"></div>').text(o.response.success.msg)
+        );
+      } // === end: display success msg
+
+    } // === end: display response
 
   }; // === func ui_ajax
-
 
 }); // === END SCOPE ====================
 
